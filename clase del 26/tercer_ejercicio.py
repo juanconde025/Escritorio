92239763bc35b8fadb69df0964627aed72c5857d
JuanDavidConde <juanconde1309@gmail.com>
2024-04-27 13:43:11 -0500

    fix: correccion ejercico 3

--- clase del 26/tercer_ejercicio.py
+++ clase del 26/tercer_ejercicio.py
@@ -7,6 +7,9 @@ def volumen_cilindro(altura, area_c):
     volumen = area_c * altura
     return print(f"el volumen del cilindro es ", volumen)
 
+
+radio = float(input("Ingrese el radio del circulo para calcular el area "))
+area_circulo(radio)
 area_c = radio* radio * 3.1416
 altura = float(input("Ingrese la altura del cilindro para calcular el volumen "))
 volumen_cilindro(altura, area_c)
